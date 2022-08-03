627584da94e4692a76ee17ffdd1a0e918a2c50f0
riwaab23-meetmeow <109904805+riwaab23-meetmeow@users.noreply.github.com>
2022-08-03 14:34:05 +0300

    weee
    i tried to add some css to the sign in/up part and it quite messed it up so now i am trying to fix it and deal with it

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -36,7 +36,7 @@ def signin():
 			return render_template("decisions.html")
 		except:
 			error = "Authentication failed"
-			return render_template("signup.html")
+			return render_template("signin.html")
 	else:
 		print("Didn't get to post. got to: {request.method}")
 		return render_template("signin.html")
@@ -46,18 +46,14 @@ def signin():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	error = ""
-	if request.method == 'POST':
-		first_name = request.form.get("first_name", False)
-		last_name = request.form.get("last_name", False)
+	if request.method == 'GET':
 		email = request.form.get("email", False)
 		password = request.form.get("password", False)
 		number = request.form.get("number", False)
-		birthday = request.form.get("birthday", False)
-		bio = request.form.get("bio", False)
 		username = request.form.get("username", False)
 		try:
 			login_session['user'] = auth.create_user_with_email_and_password(email, password)
-			user = {"first_name": first_name,"last_name ":last_name  , "email":email,"password":password, "number": number , "birthday": birthday ,"bio":bio, "username": username   }
+			user = { "email":email,"password":password, "number": number , "username": username   }
 			db.child("Users").child(login_session['user']['localId']).set(user)
 
 			return redirect(url_for('decision'))
