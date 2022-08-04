44e5eb45239d3f06683be32da9b22fd4c08c2486
riwaab23-meetmeow <109904805+riwaab23-meetmeow@users.noreply.github.com>
2022-08-04 12:47:15 +0300

    meew
    i fixed the things with the sign up/in
    it has all the requirements i added picks and stuff but the sign in/up button works but it is half a button idk how to explain it

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -10,7 +10,7 @@ config = {
 	"messagingSenderId": "748398115568",
 	"appId": "1:748398115568:web:07c539a4b68ba470b06035",
 	"measurementId": "G-XWDD4KZYPN",
-	"databaseURL":"https://console.firebase.google.com/project/meet-adcad/database/meet-adcad-default-rtdb/data/~2F"
+	"databaseURL":"https://meet-adcad-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase=pyrebase.initialize_app(config)
@@ -25,19 +25,28 @@ def hello_name_route():
     return render_template('home.html')
 
 
+@app.route('/decisions' , methods=['GET', 'POST'])
+def shrek():
+	s1="user"
+	if 'user' in login_session:
+		if login_session['user'] is not None:
+			s=db.child("Users").child(login_session['user']['localId']).get().val()
+			s1=s['username']
+	return render_template('decisions.html',name=s1)
+
+
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
 	error = ""
 	if request.method == 'POST':
-		email1 = request.form.get("emailq1", False)
+		email1 = request.form.get("email1", False)
 		password1= request.form.get("password1", False)
 		try:
 			login_session['user'] = auth.sign_in_with_email_and_password(email1, password1)
-			return render_template("decisions.html")
+			return redirect(url_for("shrek"))
 		except:
 			error = "Authentication failed"
-			return render_template("signin.html")
-
+			return render_template("signup.html")
 	return render_template("signin.html")
 
 
@@ -45,16 +54,17 @@ def signin():
 def signup():
 	error = ""
 	if request.method == 'POST':
-		print("RERFGHJ")
+		
 		email = request.form.get("email",False)
 		password = request.form.get("password",False)
 		username = request.form.get("txt",False)
+		print(username)
+		print(password)
+		print(email)
 		try:
-			print("1234567")
 			login_session['user'] = auth.create_user_with_email_and_password(email, password)
-			user = { "email":email,"password":password, "username": username   }
+			user = { "email":email,"password":password, "username": username }
 			db.child("Users").child(login_session['user']['localId']).set(user)
-
 			return redirect(url_for('decision'))
 		except:
 			error = "Authentication failed"
